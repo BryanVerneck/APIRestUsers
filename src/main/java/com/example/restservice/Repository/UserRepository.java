9a683593578091ab137a773171621c3d1f5a2a22
BryanVerneck <40044852+BryanVerneck@users.noreply.github.com>
2020-01-26 19:20:46 -0300

    first commit

---
+++ src/main/java/com/example/restservice/Repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.example.restservice.Repository;
+
+import com.example.restservice.Models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    User findById(long id);
+
+}
