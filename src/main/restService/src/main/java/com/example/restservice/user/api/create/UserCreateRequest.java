bafe89da8432ae1b01c4f0e14df9b3e78c10beac
BryanVerneck <40044852+BryanVerneck@users.noreply.github.com>
2020-01-31 13:38:06 -0300

    Add files via upload
    Package changes and tests.

---
+++ src/main/restService/src/main/java/com/example/restservice/user/api/create/UserCreateRequest.java
@@ -0,0 +1,11 @@
+package com.example.restservice.user.api.create;
+
+import java.util.Date;
+
+public class UserCreateRequest {
+    private String userName;
+    private String firstName;
+    private String lastName;
+    private Date birthDate;
+    private String email;
+}
---
+++ src/main/restService/src/test/resources/AutenticationTest.feature
@@ -0,0 +1,6 @@
+Feature: Login Feature
+
+  Scenario: Verify if a user is able to login using a valid username and password
+    Given A user is on ToolsQA login page
+    Then User is able to login successfully
+
